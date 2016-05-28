aa149284b611cc63fab58b93abd2607a6a6ced20
Masao Takaku <tmasao@acm.org>
2016-05-28 10:27:33 +0900

    add S3 Adapter for sitemap_generator

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,9 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "http://takaku-lib.herokuapp.com"
+SitemapGenerator::Sitemap.public_path = 'tmp/'
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new
+SitemapGenerator::Sitemap.sitemaps_host = "http://#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com/"
+SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
