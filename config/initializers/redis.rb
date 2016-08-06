00c70f79d807769b11820b2a4a5123509358ebc6
Masao Takaku <tmasao@acm.org>
2016-08-06 11:36:12 +0900

    change to use REDIS_URL

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,7 @@
 if ENV["REDISCLOUD_URL"]
   $redis = Resque.redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
 end
+if ENV["REDIS_URL"]
+  $redis = Resque.redis = Redis.new(:url => ENV["REDIS_URL"])
+end
 
