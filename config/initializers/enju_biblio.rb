c6a9b86e2454d115af89422b8b33420bf7d92499
Masao Takaku <tmasao@acm.org>
2016-08-26 12:17:22 +0900

    updated.

--- config/initializers/enju_biblio.rb
+++ config/initializers/enju_biblio.rb
@@ -1,27 +1,7 @@
-class Agent
+class Agent < ActiveRecord::Base
   def self.import_agents(agent_lists)
     STDERR.puts "new import_agents"
-    agents = []
-    agent_lists.each do |agent_list|
-      name_and_role = agent_list[:full_name].split('||')
-      if agent_list[:agent_identifier].present?
-        agent = Agent.where(agent_identifier: agent_list[:agent_identifier]).first
-      else
-        agent = Agent.where(full_name: name_and_role[0]).first
-      end
-      role_type = name_and_role[1].to_s.strip
-      unless agent
-        agent = Agent.new(
-          full_name: name_and_role[0],
-          full_name_transcription: agent_list[:full_name_transcription],
-          agent_identifier: agent_list[:agent_identifier],
-          language_id: 1
-        )
-        agent.required_role = Role.where(name: 'Guest').first
-        agent.save
-      end
-      agents << agent
-    end
+    agents = super(agent_lists)
     agents.uniq!
     agents
   end
