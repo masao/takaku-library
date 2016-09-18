7e2f84f0a013066f8861f5545d37a416c4aab7e5
Masao Takaku <tmasao@acm.org>
2016-09-18 09:16:17 +0000

    sitemap route: redirect to S3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, path: 'accounts'
 
+  get "sitemap", to: redirect("https://s3.amazonaws.com/takaku-library/sitemaps/sitemap.xml.gz")
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
