fb10d6dc5715790dd926f167ff1a12b8a2d1b484
Masao Takaku <tmasao@acm.org>
2018-02-24 15:26:44 +0900

    move edit_user_registration on the top.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', as: 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', as: 'user_registration'
+  end
+
   devise_for :users, path: 'accounts'
 
   authenticate :user, lambda {|u| u.role.try(:name) == 'Administrator' } do
