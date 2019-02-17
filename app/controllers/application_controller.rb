71c0f5a74f875cc1669057cc052777de1d02a56e
Masao Takaku <tmasao@acm.org>
2019-02-17 17:00:44 +0900

    omit slash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :redirect_own_domain
   def redirect_own_domain
-    redirect_to "https://takaku-lab.webcat.jp/#{request.path}", status: :moved_permanently
+    redirect_to "https://takaku-lab.webcat.jp#{request.path}", status: :moved_permanently
   end
 end
