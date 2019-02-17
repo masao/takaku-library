93eb91dbedb178ec8f3f9c49d1fc9dafd7316ca5
Masao Takaku <tmasao@acm.org>
2019-02-17 16:58:26 +0900

    updated.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  redirect_to "https://takaku-lab.webcat.jp/#{request.path}", status: :moved_permanently
+  before_filter :redirect_own_domain
+  def redirect_own_domain
+    redirect_to "https://takaku-lab.webcat.jp/#{request.path}", status: :moved_permanently
+  end
 end
